b1a55eae8800868e054be37fe4db9cd374a5701f
bclaiborne <phaylon1@gmail.com>
2019-03-06 20:08:40 -0800

    fixed sort

--- learning operators.py
+++ learning operators.py
@@ -1,24 +1,18 @@
 tree = [1,5,7,4,6,9,13,3]
 
 #sort the tree
-#compare index value to index+1
-for i in range(1, len(tree)): 
-	j = i - 1
-	value = tree[i]
-
-	while (value < tree[j]):
-		print(str(value)+ " < " +str(tree[j]))
-		j -= 1
-		if value < tree[j]:
-			tree.insert(j, value)
-		else:
-			tree.insert(i, value)
-	print("i= "+str(i)+" ,j= "+str(j)+" value= "+str(value))
+#compare index value to index-1
+for j in range(1, len(tree)): 
+	i = j - 1
+	value = tree[j]
 	
+	#compare value with the index just below and dont wrap
+	while (value < tree[i] and i > -1):
+		#if value is less than the value in the comparison slot move the compare slot to the value's original slot
+		tree[i+1] = tree[i]
+		#then move left to compare again
+		i = i-1
+	#once we have moved down i slots and the i slot value isn't > our value, write the value into the slot to the right.
+	tree[i+1] = value
 		
-	print(tree)
-		
-		
-#if i+1 > i do nothing
-#if i > i+1 remove n+1 from the list and add it before n
-
+print(tree)
